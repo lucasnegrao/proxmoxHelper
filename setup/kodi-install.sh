bc17cfe0688d6a7d30fa358d8d4bb342bd5a870c
Lucas Negrão <lucas.negrao@gmail.com>
2025-09-28 13:01:50 -0300

    Add PPA for Kodi in installation script
    Added PPA repository for Kodi installation.

--- setup/kodi-install.sh
+++ setup/kodi-install.sh
@@ -103,6 +103,8 @@ echo "/usr/sbin/lightdm" > /etc/X11/default-display-manager
 msg_ok "Installed lightdm"
 
 msg_info "Installing kodi"
+# Add PPA for Kodi (ubuntuhandbook1)
+add-apt-repository -y ppa:ubuntuhandbook1/kodi
 apt-get update &>/dev/null
 apt-get install -y kodi &>/dev/null
 set +e
