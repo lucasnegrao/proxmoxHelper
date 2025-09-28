4402675c673d4b6bc8d76fba1dc7ee62a1af477a
Lucas Negrão <lucas.negrao@gmail.com>
2025-09-28 13:44:18 -0300

    Update kodi-gbm-install.sh

--- setup/kodi-gbm-install.sh
+++ setup/kodi-gbm-install.sh
@@ -86,7 +86,13 @@ alias die='EXIT=$? LINE=$LINENO error_exit'
 set -e
     
 msg_ok "Set Up Hardware Acceleration and GBM Support"  
+msg_info "Installing Dependencies"
+DEBIAN_FRONTEND=noninteractive apt-get install -y software-properties-common curl sudo gnupg &>/dev/null
+msg_ok "Installed Dependencies"
 
+msg_info "Adding Kodi PPA"
+add-apt-repository -y ppa:ubuntuhandbook1/kodi &>/dev/null
+msg_ok "Added Kodi PPA" 
 msg_info "Setting Up kodi user"
 useradd -d /home/kodi -m kodi &>/dev/null
 gpasswd -a kodi audio &>/dev/null
@@ -97,8 +103,6 @@ gpasswd -a kodi tty &>/dev/null
 msg_ok "Set Up kodi user"
 
 msg_info "Installing kodi"
-# Add PPA for Kodi (ubuntuhandbook1)
-add-apt-repository -y ppa:ubuntuhandbook1/kodi
 apt-get update &>/dev/null
 apt-get install -y kodi &>/dev/null
 set +e
